c2f11b65a342e53ff996fdb97bf411fe8a873be3
Katie Hill <khill.fr.gr@gmail.com>
2026-04-16 16:10:59 +0100

    Add font sizes to UI styles.css & elements to dashboard

--- apps/phytovida/src/pages/Dashboard.tsx
+++ apps/phytovida/src/pages/Dashboard.tsx
@@ -1,9 +1,20 @@
+import { Button } from "@repo/ui/components/button";
+import { Link } from "react-router";
+
 
 export default function Dashboard() {
 
-	return (
-		<div className="min-h-[calc(100dvh-20dvh)] grid place-content-center">
-			Dashboard
-		</div>
-	);
+    return (
+    <div className="h-full grid place-content-center gap-6">
+      <h1 className="text-headline">Welcome, username</h1>
+      <div className="flex justify-center gap-3">
+        <Button className="rounded-full" variant="secondary" asChild>
+          <Link to="/">Growing</Link>
+        </Button>
+        <Button className="rounded-full" variant="outline" asChild>
+          <Link to="/">Planning</Link>
+        </Button>
+      </div>
+    </div>
+    );
 }
