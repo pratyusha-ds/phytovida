e504d8803b5ccbfddce84d3cebb07613392e8e0b
awynx <alwin.puche16@gmail.com>
2026-04-21 13:53:14 +0800

    feat: set up clerk and add authorization middleware for protecting protected routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,16 @@
 import "dotenv/config"; // Load environment variables from .env file
 import express from "express";
+import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import dashboardRoutes from "./routes/dashboard.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware({}));
 
 app.use("/api/dashboard", dashboardRoutes);
 
---
+++ apps/api/src/middlewares/authorize.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from "express";
+import { getAuth } from "@clerk/express";
+
+export async function authorize(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	const auth = getAuth(req);
+
+	if (!auth.isAuthenticated) {
+		res.status(403).json({
+			error: true,
+			message: "Unauthorized. Please log in to access this resource.",
+		});
+		return;
+	}
+
+	next();
+}
