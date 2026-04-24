ae65282b306c99de8f2e09f3f8080e74441cfb13
awynx <alwin.puche16@gmail.com>
2026-04-24 11:15:23 +0800

    refactor(routes): streamline API routes and enhance auth registration handling

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -5,18 +5,25 @@ import dashboardRoutes from "./dashboard.js";
 import logRoutes from "./plant-logs.js";
 import userPlantRoutes from "./user-plant.js";
 import authRoutes from "./auth.js";
+import plantRoutes from "./plantlibrary.js";
 
 // middleware
 import { authGuard } from "../middlewares/authGuard.js";
 
 const router: Router = Router();
 
+// auth routes
 router.use("/auth", authRoutes);
 
+// dashboard routes
 router.use("/dashboard", dashboardRoutes);
 
-// user plants
-router.use("/my-plants", /* authGuard */ userPlantRoutes); // TODO: uncomment authGuard middleware IF user have plants data
-router.use("/my-plants/:plantId/logs", /* authGuard */ logRoutes); // TODO: uncomment authGuard middleware IF user have plants data
+// plants routes
+router.use("/plants", plantRoutes);
+
+// user plants routes
+router.use("/my-plants", authGuard, userPlantRoutes);
+
+router.use("/my-plants/:plantId/logs", authGuard, logRoutes);
 
 export default router;
